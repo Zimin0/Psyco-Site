47e2b4efbed709fbc135d184f41657efa906d5f9
Zimin0 <you@example.com>
2024-01-19 16:07:08 +0300

    MEDIA_URL, STATIC_...

--- psycosite/settings.py
+++ psycosite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,8 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
 
 
 # Application definition
@@ -40,7 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'article',
+    'services',
+    'workdirections'
 ]
 
 MIDDLEWARE = [
