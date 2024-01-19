fcbf4c48222263e3500761657b8e9f0c11816067
Zimin0 <you@example.com>
2024-01-19 16:03:49 +0300

    Add services page

--- services/urls.py
+++ services/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from services.views import *
 
-from home.views import *
+app_name = 'services'
 
 urlpatterns = [
     path('services-and-prices/', services, name="services")    
--- services/views.py
+++ services/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def services(request):
     """ Услуги и цены """
-    ...
+    context = {}
+    return render(request, 'services/services.html', context)
