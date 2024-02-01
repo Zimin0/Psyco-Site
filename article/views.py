5d00b0cb9ef1308cf1329221b0c293f55f80ebf0
Zimin0 <you@example.com>
2024-02-01 21:58:07 +0300

    Add articles

--- article/views.py
+++ article/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
 from article.models import Article
+from settings.upload_info import get_personal_data_from_db
 
 def article(request, id):
     """ Статья. """
     context = {}
+    context.update(get_personal_data_from_db())
     article = get_object_or_404(Article, id=id)
     context['article'] = article
     return render(request, 'article/article.html', context)
