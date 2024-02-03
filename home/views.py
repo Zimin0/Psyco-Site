f1dbecafb2e750f68b2a9498dea8fbc29d52940a
Zimin0 <you@example.com>
2024-02-03 14:10:22 +0300

    Add cooperation page

--- home/views.py
+++ home/views.py
@@ -14,19 +14,23 @@ def home(request):
 def about(request):
     """ Обо мне. """
     context = {}
+    context.update(get_personal_data_from_db())
     return render(request, 'home/about.html', context)
 
 def work_directions(request):
     """ Направления работы. """
     context = {}
+    context.update(get_personal_data_from_db())
     return render(request, 'home/work_directions.html', context)
 
 def cooperation(request):
     """ Сотрудничество. """
     context = {}
-    return render(request, 'home/cooperation.html', context)
+    context.update(get_personal_data_from_db())
+    return render(request, 'home/coopetation.html', context)
 
 def contacts(request):
     """ Контакты. """
     context = {}
+    context.update(get_personal_data_from_db())
     return render(request, 'home/contacts.html', context)
