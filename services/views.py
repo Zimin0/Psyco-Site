c363bcecbfae8e876f769d86057b083f1a2496d5
Zimin0 <you@example.com>
2024-02-03 19:36:44 +0300

    Add services

--- services/views.py
+++ services/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from services.models import Service
+from settings.upload_info import get_personal_data_from_db
 
 
 def services(request):
@@ -8,4 +9,5 @@ def services(request):
     context = {
         'services': Service.objects.filter()
     }
+    context.update(get_personal_data_from_db())
     return render(request, 'services/services.html', context)
